529eedc3826e4823a2fc13202a98e160d55e076b
unknown <sammottley@gmail.com>
2015-11-14 20:09:37 +0000

    more tweaks

--- carriage_hub/carriage_hub.ino
+++ carriage_hub/carriage_hub.ino
@@ -23,8 +23,9 @@ EthernetClient client;
 /**
  * Hub unique information
  */
-const uint8_t key[] = {'t','z','G','N','3','n','t','q','y','W','S','I','Z','M','L','6','n','Y','Y','H','e','B','2','E','p','D','k','2','I','H','9','2'};
+//const uint8_t key[] = {'t','z','G','N','3','n','t','q','y','W','S','I','Z','M','L','6','n','Y','Y','H','e','B','2','E','p','D','k','2','I','H','9','2'};
 const char username[] = "hub_225468";
+const char enc_key[] = "tzGN3ntqyWSIZML6nYYHeB2EpDk2IH92";
 char password[] = "password";
 const char api_key[] = "8zA4N3vDrhgEFQugX4ThtO1Ch";
 
@@ -34,6 +35,7 @@ const char api_key[] = "8zA4N3vDrhgEFQugX4ThtO1Ch";
  */
 unsigned long lastConnectionTime = 0;             // last time you connected to the server, in milliseconds
 const unsigned long postingInterval = 100L * 1000L; // delay between updates, in milliseconds
+boolean connected_to = false;
 
 
 /**
@@ -62,13 +64,19 @@ void setup() {
  * Main program loop
  */
 void loop() {
+    
    if (client.available()) {
       char c = client.read();
+      connected_to = true;
       Serial.print(c);
+   }else{
+      connected_to = false;
    }
-    
-   if (millis() - lastConnectionTime > postingInterval) {
-     httpRequest();
+   
+   if(connected_to == false){
+     if (millis() - lastConnectionTime > postingInterval) {
+       httpRequest();
+     }
    }
 }
 
@@ -82,8 +90,13 @@ void httpRequest(){
  if (client.connect(server, 80)) {
     Serial.println("connected");
     // Make a HTTP request:
-    client.println("GET / HTTP/1.1");
+    client.println("GET /api/v1/config/"+String(api_key)+" HTTP/1.1");
     client.println("Host: 192.168.33.17");
+    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.println("api: "+String(api_key));
+    client.println("enc: "+String(enc_key));
+    client.println("username: "+String(username));
+    client.println("Password: "+String(password));
     client.println("Connection: close");
     client.println();
     
@@ -92,4 +105,25 @@ void httpRequest(){
     Serial.println("connetion error");
   }
   
+}
+
+
+String URLEncode(const char* msg)
+{
+    const char *hex = "0123456789abcdef";
+    String encodedMsg = "";
+
+    while (*msg!='\0'){
+        if( ('a' <= *msg && *msg <= 'z')
+                || ('A' <= *msg && *msg <= 'Z')
+                || ('0' <= *msg && *msg <= '9') ) {
+            encodedMsg += *msg;
+        } else {
+            encodedMsg += '%';
+            encodedMsg += hex[*msg >> 4];
+            encodedMsg += hex[*msg & 15];
+        }
+        msg++;
+    }
+    return encodedMsg;
 }

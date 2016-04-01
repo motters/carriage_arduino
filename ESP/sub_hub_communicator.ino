b70f8582d2ff404a9853221fe9883d6d7694b44f
Sam Mottley <sammottley@gmail.com>
2016-04-01 23:12:45 +0100

    Update sub_hub_communicator.ino

--- ESP/sub_hub_communicator.ino
+++ ESP/sub_hub_communicator.ino
@@ -9,62 +9,81 @@
 #include <ArduinoJson.h>
 
 
-
 /** 
  * Set the main hub wireless settings
  * this will be printed on the side of the device and unique to each sub hub
  */
 const char *ssid = "SUBHUB-API-KEY";
 const char *password = "SUBHUB-PASSWORD";
-bool main_hub = false;
+
 
 /**
  * Misc global vars
  */ 
-communicator* nodeCommunicator = new communicator(true, String(ssid), String(password));
-
+communicator* nodeCommunicator = new communicator(true, String(ssid), String(password)); // Communicator class
+String data_buffer = ""; // Buffer to hold data
+int data_buffer_length = 1000; // Allowed length of data buffer
+bool main_hub = false; // Has the sub hub been configured by main hub?
 
 
 /** 
  * Setup the device
  */
-void setup() {
+void setup()
+{
 	// Setup serial and allow device to settle
 	delay(1000);
 	Serial.begin(115200);
-        Serial.println("Setup loop finished");
+    //Serial.setDebugOutput(true);
 }
 
 
 /** 
  * Setup the device
  */
-void loop() {
-
+void loop() 
+{
 	// If the main hub connection details are not collected then handle clients
-	if(!main_hub){
+	if(!main_hub)
+	{
 		// Wait for main hub
-		if(nodeCommunicator->configureSubHub()){
+		if(nodeCommunicator->configureSubHub())
+		{
 			main_hub = true;  
-                        Serial.println("Sub hub configured");
-                }
+            Serial.println("connection_details_recieved");
+        }
 	}
 
 	// If main hub has been defined
-	if(main_hub){
+	if(main_hub)
+	{
 		// Wait for serial to be sent by sub hub (ardunio)
-		if(Serial.available() > 0){
-			// Read the data ready to be sent to main hub
+		if(Serial.available() > 0)
+		{
+			// Read the data
 			String data = Serial.readString();
-                        Serial.print("Data to send: ");
-                        Serial.println(data);
-			// Send the data
-                        bool sent = false;
-                        while(!sent){
-                               sent = nodeCommunicator->toHub(data);
-                               delay(1000);
-                        }
-                        Serial.println("Data sent successfully");
+
+			// Add new data to buffer
+			data_buffer += data;
+
+			//Reply
+			Serial.println("data_recieved");
+
+			// Should the buffer be sent to the main hub?
+            if(data_buffer.length() >= data_buffer_length)
+			{
+				// Send the data
+				bool sent = false;
+				while(!sent)
+				{
+					sent = nodeCommunicator->toHub(String(ssid)+"}"+data_buffer);
+					delay(1000);
+				}
+
+				// Reply
+				Serial.println("data_sent");
+			}			
 		}
 	}
 }
+

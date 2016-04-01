bdb9656f178fc595082f196dbb81695885b03eca
Sam Mottley <sammottley@gmail.com>
2016-04-01 22:21:30 +0100

    Update carriage_hub.ino
    Updates to work with improved data structure and implements upload

--- carriage_hub/carriage_hub.ino
+++ carriage_hub/carriage_hub.ino
@@ -7,8 +7,6 @@
 #include <ArduinoJson.h>
 
 
-
-
 /**
  * Setup ethernet connection
  *
@@ -17,7 +15,7 @@
 // Set mac address
 byte mac[] = { 0x90, 0xA2, 0xDA, 0x0F, 0x19, 0x28 };
 // Set the web panel ip
-char server[] = "192.168.0.171";
+char server[] = "192.168.0.171"; // 192.168.0.171
 // Set the static IP address to use if the DHCP fails to assign
 IPAddress ip(192, 168, 135, 1);
 // Initialize the Ethernet client library
@@ -30,7 +28,7 @@ EthernetClient client;
  * @author Sam Mottley
  */
 const char username[] = "hub_225468";
-const char enc_key[] = "tzGN3ntqyWSIZML6nYYHeB2EpDk2IH92";
+const char enc_key[] = "tzGN3ntqyWSIZML6nYYHeB2EpDk2IH92"; 
 char password[] = "password";
 const char api_key[] = "8zA4N3vDrhgEFQugX4ThtO1Ch";
 String serial_config = "";
@@ -70,7 +68,9 @@ void setup()
   }
 
   // Get the hubs and associated sub hubs configuration
-  serial_config = httpRequestConfig();
+  while(serial_config == ""){
+	  serial_config = httpRequestConfig();
+  }
   cstr = new char[serial_config.length() + 1];
   strcpy(cstr, serial_config.c_str());
 
@@ -84,33 +84,35 @@ void setup()
  */
 void loop()
 {
-  // De code system settings (dynamic buffer size still needs to be sorted)
-  Serial.println(serial_config);
-  DynamicJsonBuffer jsonBuffer;
-  JsonObject& root = jsonBuffer.parseObject(cstr);
-  char module = 0 ;
-  JsonArray& modules = root["sub_hubs"]["okpo"]["modules"];
-  
-  const char* sub_hub_demo_api = root["sub_hubs"]["okpo"]["api_key"];
+	// Set data to send to sub hubs     EG    {"sub_hubs":[{"s":"sub_v1_1","p":"password","m":[{"t":"2","in":"8000","mc":"SH-D-3"},{"t":"2","in":"5000","mc":"SH-I2C-105"}]}]}
+	String Hubs = "[{\"s\": \""+String(api_key)+"\", \"p\":\""+String(password)+"\"}, ";
+	String SubHubs = cstr; SubHubs.remove(0,1);  SubHubs = Hubs + SubHubs;
 
-  // Loop forever now config settings are loaded
-  while (true)
-  {
-    // temp
-    //Serial.println(sub_hub_demo_api);
-    
-    modules.printTo(Serial); 
-    Serial.println("");
-    root.printTo(Serial);    
-    delay(5000);
-    // Check for incomming requests
-    // Check sub hub is in the config array
-    // proive the sub hubs configuration OR save the module data from sub hub to buffer
+	// Send data to wirless module
+	Serial.println(SubHubs);
 
-    // Check the size of the module data buffer
-    // if greated than x then upload the buffered module data to the web server
+	// Variable to store incomming information
+	String data = "";
 
-  }
+	// Loop forever now config settings are loaded
+	while (true)
+	{
+		// Check for incomming requests
+		if(Serial.available() > 0)
+		{
+			// Read the data
+			data = Serial.readString();
+
+			// Upload data to web sever
+			boolean uploaded = false;
+			while(!uploaded){
+				uploaded = httpRequestUpload(data);
+			}
+
+			// Clear data from memory
+			data = "";
+		}
+	}
 
 }
 
@@ -118,10 +120,41 @@ void loop()
 
 /**
  * Upload the data from the module buffer to the web server
+ *
  */
-boolean httpRequestUpload()
+boolean httpRequestUpload(String data_string)
 {
+  // Ensure socket is not in use
+  if (!client.connected()) {
+    client.stop();
+  }
 
+  // When socket free open a connection
+  if (client.connect(server, 80)) {
+	// Make the http request to server     ?data="+data_string+"
+    client.println("POST /api/v1/upload/" + String(api_key) + "?data="+data_string+" HTTP/1.1\nHost: 192.168.0.171\napi: " + String(api_key) + "\nenc: " + String(enc_key) + "\nusername: " + String(username) + "\npassword: " + String(password) + "\nContent-Type: multipart/form-data;\nConnection: close");
+    client.println();
+  }
+  
+  // Wait for server to recieve request
+  while (client.connected() && !client.available()) delay(1);
+
+  // Retrieve data from server and store in buffer
+  String data = "";
+  while (client.connected() || client.available()) {
+    char c = client.read();
+    //Serial.print(c);
+    if (c == '\n') {
+      data += ' ';
+    } else {
+      data += c;
+    }
+  }
+
+  // Close the connection / socket
+  client.stop();
+
+  return true;
 }
 
 
@@ -141,11 +174,11 @@ String httpRequestConfig()
   // When socket free open a connection
   if (client.connect(server, 80)) {
     // Make the http request to server
-    client.println("GET /api/v1/config/" + String(api_key) + " HTTP/1.1\nHost: 192.168.33.17\napi: " + String(api_key) + "\nenc: " + String(enc_key) + "\nusername: " + String(username) + "\npassword: " + String(password) + "\nConnection: close");
+    client.println("GET /api/v1/config/" + String(api_key) + " HTTP/1.1\nHost: 192.168.0.171\napi: " + String(api_key) + "\nenc: " + String(enc_key) + "\nusername: " + String(username) + "\npassword: " + String(password) + "\nConnection: close");
     //client.println("GET /test HTTP/1.1\nHost: 192.168.33.17\napi: "+String(api_key)+"\nenc: "+String(enc_key)+"\nusername: "+String(username)+"\npassword: "+String(password)+"\nConnection: close");
     client.println();
   }
-
+  
   // Wait for server to recieve request
   while (client.connected() && !client.available()) delay(1);
 
@@ -164,7 +197,7 @@ String httpRequestConfig()
   client.stop();
 
   // Take only the content not the headers
-  const char *PATTERN1 = "sub_hubs";
+  const char *PATTERN1 = " [{\"s\":\"";
   const char *PATTERN2 = " 0";
 
   char *target = NULL;
@@ -178,38 +211,17 @@ String httpRequestConfig()
       target = ( char * )malloc( end - start + 1 );
       memcpy( target, start, end - start );
       target[end - start] = '\0';
-    }
+    }else{
+		return "";
+	}
+  }else{
+	  return "";
   }
 
   // Format the data and ensure valid json
-  data = "{\"sub_hubs" + String(target);
-  Serial.println(data);
+  data = "[{\"s\":\"" + String(target);
   free( target );
   return data;
 }
 
 
-/**
- * Encode data being sent over http connection
- *
- * @author Sam Mottley
- */
-String URLEncode(const char* msg)
-{
-  const char *hex = "0123456789abcdef";
-  String encodedMsg = "";
-
-  while (*msg != '\0') {
-    if ( ('a' <= *msg && *msg <= 'z')
-         || ('A' <= *msg && *msg <= 'Z')
-         || ('0' <= *msg && *msg <= '9') ) {
-      encodedMsg += *msg;
-    } else {
-      encodedMsg += '%';
-      encodedMsg += hex[*msg >> 4];
-      encodedMsg += hex[*msg & 15];
-    }
-    msg++;
-  }
-  return encodedMsg;
-}
